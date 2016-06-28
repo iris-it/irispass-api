d8c4332776cbfbb7bf72a0f6ae41a42b8189adb2
Alexandre <alexandre.mangin@viacesi.fr>
2016-06-28 16:11:48 +0900

    revert

--- config/filesystems.php
+++ config/filesystems.php
@@ -48,6 +48,11 @@ return [
             'root' => storage_path('app'),
         ],
 
+        'osjs' => [
+            'driver' => 'local',
+            'root' => env('OSJS_VFS_PATH'),
+        ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
